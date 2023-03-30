43ded488f5ed455e403787bb463839bc84c9aa35
nou-man <4pa20cs059@pace.edu.in>
2023-03-30 13:01:26 +0530

    todo post set

--- server/index.js
+++ server/index.js
@@ -12,6 +12,21 @@ app.use(express.json());
 
 //create a todo 
 
+app.post("/todos", async(req,res) => {
+    try {
+        const {description} = req.body
+        const newTodo = await pool.query(
+            "INSERT INTO todo (description) VALUES($1) RETURNING *",
+            [description]
+            );
+        
+        res.json(newTodo.rows[0]);
+        
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
 //get all todo
 
 //get a todo
@@ -23,3 +38,4 @@ app.use(express.json());
 app.listen(port, () => {
     console.log("The server has started on port 5000");
 });
+
