99961d916fd4721af5e4e0929ae07b1293c9f895
nou-man <4pa20cs059@pace.edu.in>
2023-04-03 13:41:05 +0530

    added:deleteFeature

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -3,6 +3,19 @@ import React, { Fragment, useEffect, useState } from "react";
 export default function ListTodos() {
   const [todos, setTodos] = useState([]);
 
+  //delete function
+
+  const deleteTodo = async (id) => {
+    try {
+      const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+        method: "DELETE",
+      });
+      setTodos(todos.filter(todo => todo.todoid !== id));
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
   const getTodos = async () => {
     try {
       const response = await fetch("http://localhost:5000/todos");
@@ -17,7 +30,7 @@ export default function ListTodos() {
   useEffect(() => {
     getTodos();
   }, []);
-//   console.log(todos);
+  //   console.log(todos);
   return (
     <Fragment>
       <table className="table table-striped mt-5 text-center">
@@ -34,11 +47,16 @@ export default function ListTodos() {
             <td>Doe</td>
             <td>john@example.com</td>
           </tr>} */}
-          {todos.map(todo => (
-            <tr>
-                <td>{todo.description}</td>
-                <td>Edit</td>
-                <td>Delete</td>
+          {todos.map((todo) => (
+            <tr key={todo.todoid}>
+              <td>{todo.description}</td>
+              <td>Edit</td>
+              <td
+                className="btn btn-danger"
+                onClick={() => deleteTodo(todo.todoid)}
+              >
+                Delete
+              </td>
             </tr>
           ))}
         </tbody>
